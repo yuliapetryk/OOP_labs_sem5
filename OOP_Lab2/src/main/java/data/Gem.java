938bf7e0760669736e4c810e2101596b4e929310
Yulia Petryk <yulya.petruk.03@gmail.com>
2023-11-23 13:32:01 +0200

    created class Gem

---
+++ OOP_Lab2/src/main/java/data/Gem.java
@@ -0,0 +1,51 @@
+package data;
+
+import com.sun.jdi.Value;
+
+public class Gem {
+    private String name;
+    private String preciousness;
+    private String origin;
+    private VisualParameters visualParameters;
+    private double value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPreciousness() {
+        return preciousness;
+    }
+
+    public void setPreciousness(String preciousness) {
+        this.preciousness = preciousness;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public VisualParameters getVisualParameters() {
+        return visualParameters;
+    }
+
+    public void setVisualParameters(VisualParameters visualParameters) {
+        this.visualParameters = visualParameters;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}

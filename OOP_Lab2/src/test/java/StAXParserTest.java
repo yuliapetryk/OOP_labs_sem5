e210956c4175768114d12c740d29a4825293e499
Yulia Petryk <yulya.petruk.03@gmail.com>
2023-11-24 01:57:47 +0200

    added tests for StAX parser

---
+++ OOP_Lab2/src/test/java/StAXParserTest.java
@@ -0,0 +1,36 @@
+import data.Gem;
+import data.VisualParameters;
+
+import javax.xml.stream.XMLStreamException;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import parsers.StAXParser;
+
+public class StAXParserTest {
+    private StAXParser parser;
+    @Test
+    public void testRunStAXParser() throws  IOException, XMLStreamException {
+        File file = new File("src/main/resources/gem.xml");
+
+        List<Gem> expectedGemList = Arrays.asList(
+                new Gem("Diamond", 1, "precious", "South Africa", 3.0,
+                        new VisualParameters("colorless", 0.9, 57)),
+                new Gem("Amethyst", 2, "semi-precious", "Brazil", 8.0,
+                        new VisualParameters("purple", 0.7, 33)),
+                new Gem("Citrine", 3, "semi-precious", "Brazil", 1.2,
+                        new VisualParameters("yellow", 0.85, 38)),
+                new Gem("Ruby", 4, "precious", "Myanmar", 2.5,
+                        new VisualParameters("red", 0.95, 50))
+        );
+
+        parser= new StAXParser();
+        List<Gem> actualGemList = parser.runStAXParser(file);
+        Assertions.assertEquals(expectedGemList.toString(), actualGemList.toString());
+    }
+}
+

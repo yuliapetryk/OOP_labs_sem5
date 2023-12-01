9aadd15c912b22070e81c5f4c119ef83aee54d0d
Yulia Petryk <yulya.petruk.03@gmail.com>
2023-12-01 22:21:32 +0200

    add play function

--- OOP_Lab3/app/src/main/java/com/example/mazegame/MainActivity.java
+++ OOP_Lab3/app/src/main/java/com/example/mazegame/MainActivity.java
@@ -2,16 +2,22 @@ package com.example.mazegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void play (View view) {
+        Intent intent = new Intent(MainActivity.this, GameActivity.class);
+        startActivity(intent);
+        finish();
     }
 }

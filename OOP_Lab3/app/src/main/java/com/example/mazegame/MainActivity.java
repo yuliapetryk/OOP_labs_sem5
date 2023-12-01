f179337d1248b595bf99289fe400e1ba36b6418b
Yulia Petryk <yulya.petruk.03@gmail.com>
2023-12-01 21:25:58 +0200

    create android project

---
+++ OOP_Lab3/app/src/main/java/com/example/mazegame/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.mazegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

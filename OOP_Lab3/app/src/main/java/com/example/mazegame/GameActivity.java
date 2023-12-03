b73c80b162497692f8e93bc63856885ad0376e91
Yulia Petryk <yulya.petruk.03@gmail.com>
2023-12-03 17:52:55 +0200

    added a drawing of the path

--- OOP_Lab3/app/src/main/java/com/example/mazegame/GameActivity.java
+++ OOP_Lab3/app/src/main/java/com/example/mazegame/GameActivity.java
@@ -1,92 +1,137 @@
 package com.example.mazegame;
 
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.util.DisplayMetrics;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class GameActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+import java.util.ArrayList;
+import java.util.Arrays;
 
-    GameThread gameThread;
-    SurfaceHolder surfaceHolder;
-    private SurfaceView surfaceView;
-    MediaPlayer btn_click;
+public class GameActivity extends AppCompatActivity  {
+
+    public enum Color {
+        WHITE, GRAY, BLACK;
+    }
+    public MazeView mMazeView;
+    public FingerLine mFingerLine;
+    static ImageView home;
+    ImageView arrow;
+    public int mazeSize;
+    DisplayMetrics displaymetrics = new DisplayMetrics();
+    FrameLayout mFrameLayout;
+    public final int PADDING = 64;
+    public final int FAT_FINGERS_MARGIN = 25;
+
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    public  void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        //btn_click = MediaPlayer.create(this, R.r)
-        surfaceView = findViewById(R.id.surfaceView);
-        surfaceView.getHolder().addCallback(this);
-        surfaceView.setFocusable(true);
-        AppConstants.initialization(GameActivity.this);
-    }
+        this.getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
 
-    @Override
-    public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
-        this.surfaceHolder = surfaceHolder;
-        gameThread = new GameThread(surfaceHolder);
-        if (!gameThread.isRunning()){
-            gameThread = new GameThread(surfaceHolder);
-            gameThread.start();
-        } else{
-            gameThread.start();
-        }
+        mFrameLayout = (FrameLayout)findViewById(R.id.mazeWrapper);
+        ViewGroup.LayoutParams params = mFrameLayout.getLayoutParams();
+        params.height = (int) Math.floor(displaymetrics.heightPixels * 0.7);
+        mFrameLayout.setLayoutParams(params);
 
-    }
-
-    @Override
-    public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
-        AppConstants.surfaceViewWidth = surfaceView.getWidth();
-        AppConstants.surfaceViewHeight = surfaceView.getHeight();
-    }
-
-    @Override
-    public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
-
-        gameThread.setIsRunning(false);
-        boolean retry = true;
-        while (retry){
-            try {
-                gameThread.join();
-                retry = false;
-            } catch (InterruptedException e){
-
+        ImageButton newMazeButton =  findViewById(R.id.newMazeButton);
+        newMazeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                createMaze();
             }
-        }
+        });
+        newMazeButton.performClick();
+
     }
 
-    public  void addStep(View view){
-        if (btn_click != null){
-            btn_click.start();
-        }
-        String movement = view.getTag().toString();
-        switch (movement){
-            case "top":
-                if(!AppConstants.getGameEngine().currentPositinY.equals(0))
-                AppConstants.getGameEngine().currentPositinY -=1;
-                break;
-            case "bottom":
-                if(!AppConstants.getGameEngine().currentPositinY.equals(AppConstants.maxY))
-                    AppConstants.getGameEngine().currentPositinY +=1;
-                break;
-            case "right":
-                if(!AppConstants.getGameEngine().currentPositinX.equals(AppConstants.maxX))
-                    AppConstants.getGameEngine().currentPositinX +=1;
-                break;
-            case "left":
-                if(!AppConstants.getGameEngine().currentPositinX.equals(0))
-                    AppConstants.getGameEngine().currentPositinX -=1;
-                break;
+    public void createMaze() {
 
+        if (mMazeView != null) {
+            ((ViewGroup) mMazeView.getParent()).removeView(mMazeView);
+        }
+        if (mFingerLine != null) {
+            ((ViewGroup) mFingerLine.getParent()).removeView(mFingerLine);
+        }
+        mazeSize = 10;
+        mMazeView = new MazeView(this, mazeSize);
+
+
+        int[][] solutionAreas = new int[mMazeView.lengthOfSolutionPath][4];
+
+        int currentVertexKey;
+        int totalMazeWidth = displaymetrics.widthPixels - PADDING;
+        int cellSide = totalMazeWidth / mazeSize;
+        int row, column;
+        int topLeftX, topLeftY, bottomRightX, bottomRightY;
+
+        for (int i = 0; i < mMazeView.lengthOfSolutionPath; i++) {
+
+            currentVertexKey = mMazeView.listOfSolutionVertecesKeys[i];
+            row = (currentVertexKey) / mazeSize;
+            column = (currentVertexKey) % mazeSize;
+            topLeftX = (PADDING / 2) + (column * cellSide) - FAT_FINGERS_MARGIN;
+            topLeftY = (PADDING / 2) + (row * cellSide) - FAT_FINGERS_MARGIN;
+            bottomRightX = (PADDING / 2) + ((column + 1) * cellSide) + FAT_FINGERS_MARGIN;
+            bottomRightY = (PADDING / 2) + ((row + 1) * cellSide) + FAT_FINGERS_MARGIN;
+            solutionAreas[i] = new int[]{ topLeftX, topLeftY, bottomRightX, bottomRightY };
+        }
+
+        mFrameLayout.addView(mMazeView);
+        mFingerLine = new FingerLine(this, null, solutionAreas);
+        mFrameLayout.addView(mFingerLine);
+
+        int startCellArrowX = solutionAreas[mMazeView.lengthOfSolutionPath - 1][0] + 12 + FAT_FINGERS_MARGIN;
+        int startCellArrowY = solutionAreas[mMazeView.lengthOfSolutionPath - 1][1] + 100 + FAT_FINGERS_MARGIN;
+        arrow = (ImageView) findViewById(R.id.arrow);
+        arrow.setX(startCellArrowX);
+        arrow.setY(startCellArrowY);
+        arrow.setVisibility(View.VISIBLE);
+
+
+        int endCellHomeX = solutionAreas[0][0] + 8 + FAT_FINGERS_MARGIN;
+        int endCellHomeY = solutionAreas[0][1] + 10 + FAT_FINGERS_MARGIN;
+        home = (ImageView) findViewById(R.id.home);
+        home.setX(endCellHomeX);
+        home.setY(endCellHomeY);
+        home.setVisibility(View.VISIBLE);;
+    }
+
+
+    public static Integer[] removeValueFromArray(Integer[] array, Integer value) {
+
+        ArrayList<Integer> arrayList = new ArrayList<Integer>(Arrays.asList(array));
+        arrayList.remove(arrayList.indexOf(value));
+
+        Integer[] newArray = new Integer[arrayList.size()];
+        arrayList.toArray(newArray);
+
+        return newArray;
+    }
+
+    public static void startGameSolvedAnimation() {
+        final Animation animation = new AlphaAnimation(1, 0);
+        animation.setDuration(700);
+        animation.setInterpolator(new LinearInterpolator());
+        animation.setRepeatCount(3);
+        animation.setRepeatMode(Animation.REVERSE);
+        home.startAnimation(animation);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            default:
+                return super.onOptionsItemSelected(item);
 
         }
     }
